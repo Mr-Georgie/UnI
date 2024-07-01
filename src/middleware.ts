5d241d3ef6d06a00ab08491f197ce871fa32e582
Mr-Georgie <george.isiguzo@yahoo.com>
2024-07-01 20:16:53 +0100

    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,9 @@
-export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+    pages: {
+        signIn: "/auth/signin", // Redirect to the custom sign-in page
+    },
+});
 
 export const config = { matcher: ["/dashboard"] };
